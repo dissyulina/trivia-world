5791d7b4accbd1e8763ec3f478139c462147ee14
Dissy Ulina <dissyulina@gmail.com>
2021-07-14 10:48:05 +0000

    Add getting result function from the wheel in Wheel page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,33 @@
+//The script for spinning wheel was adapted and changed from youtube Weibenfalk - Vanilla Javascript Wheel of Fortune
 // Immediately invoked function expression
 (function () {
 	const wheel = document.querySelector('#wheel');
 	const buttonSpin = document.querySelector('#btn-wheel');
+	const modalTopic = document.querySelector('#modal-topic')
+	const topicName = document.querySelector('#modal-title');
 	let deg = 0;
+	let sectionDeg = 30;
+
+	const sectionNames = {
+		1: "Science",
+		2: "History",
+		3: "Music",
+		4: "Art",
+		5: "Geography",
+		6: "Sports",
+		7: "Science",
+		8: "History",
+		9: "Music",
+		10: "Art",
+		11: "Geography",
+		12: "Sports",
+	}
+	/*
+	const handleResult = (actualDeg) => {
+		
+	}
+	*/
+	$("#modal-topic").modal("hide");
 
 	// Create a spin between 3000 - 6000 degree
 	buttonSpin.addEventListener('click', () => {
@@ -17,9 +42,18 @@
 	wheel.addEventListener('transitionend', () => {
 		buttonSpin.style.pointerEvents = 'auto';
 		wheel.classList.remove('blur');
-		// Calculate the actual deg (0-360deg) and transform the wheel instantly without the user seeing it
+		// Calculate the actual deg (0-360deg), because we want the next spin to start from that degree.
+		// and transform the wheel instantly without the user seeing it
 		const actualDeg = deg % 360;
 		wheel.style.transition = 'none';
 		wheel.style.transform = `rotate(${actualDeg}deg)`;
+		// Calculate and display the result
+		//handleResult(actualDeg);
+		$('#modal-topic').appendTo("body").modal('show');
+		//$("#modal-topic").modal("show");
+		//modalTopic.modal("show");
+		const result = Math.ceil(actualDeg / sectionDeg);
+		topicName.innerHTML = sectionNames[result];
+
 	});
 })();
