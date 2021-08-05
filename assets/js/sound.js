3563652c52663bb045a821392a9d52829e9d9173
Dissy Ulina <dissyulina@gmail.com>
2021-08-05 10:26:48 +0000

    Add script to toggle sound on and off

--- assets/js/sound.js
+++ assets/js/sound.js
@@ -3,26 +3,50 @@
 let sounds = document.getElementsByTagName("audio");
 let toggleSound = document.getElementsByClassName("toggle-sound");
 console.log(toggleSound);
-let i; //increment for audio
+let getSound = sessionStorage.getItem("sound");
+
+let i; //increment for audio tag
 let j; //increment for toggle sound
 
+console.log("sound for this page is :" + getSound);
+// If toggle sound is clicked
 $(".toggle-sound").click(function() {
-    if(sounds[0].muted){
-        console.log("toggle button is clicked to play sound");
-        for(i = 0; i < sounds.length; ++i) {
-            sounds[i].muted = false;
-        }
-        for(j=0; j < toggleSound.length; j++) {
-            toggleSound[j].innerHTML = '<i class="fas fa-volume-down"></i>';
-        }
-    } else {
+    getSound = sessionStorage.getItem("sound");
+    console.log("sound is : " + getSound);
+    if ((getSound === "on") || (getSound === null)) {
+        sessionStorage.setItem("sound", "off");
         console.log("toggle button is clicked to mute");
-        for(i = 0; i < sounds.length; ++i) {
-            sounds[i].muted = true;
-        }
-        for(j=0; j < toggleSound.length; j++) {
-            toggleSound[j].innerHTML = '<i class="fas fa-volume-mute"></i>';
-        }
+        soundOff();
+    } else {
+        sessionStorage.setItem("sound", "on");
+        console.log("toggle button is clicked to play sound");
+        soundOn();
     }
 });
 
+//Set sound when loading a new page
+if ((getSound === "on") || (getSound === null)) {
+    soundOn();
+} else {
+    soundOff();
+}
+
+//Function when sound is off
+function soundOff() {
+    for(i = 0; i < sounds.length; ++i) {
+        sounds[i].muted = true;
+    }
+    for(j=0; j < toggleSound.length; j++) {
+        toggleSound[j].innerHTML = '<i class="fas fa-volume-mute"></i>';
+    }
+}
+
+//Function when sound is on
+function soundOn() {
+    for(i = 0; i < sounds.length; ++i) {
+        sounds[i].muted = false;
+    }
+    for(j=0; j < toggleSound.length; j++) {
+        toggleSound[j].innerHTML = '<i class="fas fa-volume-down"></i>';
+    }
+}
