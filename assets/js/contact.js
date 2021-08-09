3ced4cb71d80afff36b0c6d9239671079a6e07e2
Dissy Ulina <dissyulina@gmail.com>
2021-08-09 19:42:27 +0000

    Add a loader to Send Message button in Contact page

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,18 +1,26 @@
-// Using emailJS, send email
-//$('#btn-submit').click(function (event) {
-$('#contact-form').on('submit', function (e) {
+// JS for Contact page
 
+$('#contact-form').on('submit', function (e) {
     e.preventDefault();
 
+    // Display a loader for 1 second
+    $("#btn-submit").html(
+        `<div id="loader">
+            <img src="assets/images/spinner.gif" alt="loading" height="30px">
+        </div>`
+    );
+    
+    // After 1 second, turn the text inside the button to Sent
     setTimeout(function() {
         $("#btn-submit").html('<i class="fas fa-check"></i> Sent');
         $("#btn-submit i").css("color", "#01a74b");
         $('#contact-form')[0].reset();
     }, 1000);
     
-    //sendMail(this); 
+    sendMail(this); 
 });
 
+// Using emailJS, send email to developer everytime a form is submitted
 function sendMail(contactForm) {
     emailjs.send("gmail", "template_trivaworld", {
         "from_name": contactForm.name.value,
@@ -24,9 +32,5 @@ function sendMail(contactForm) {
     }, function(error) {
         console.log('FAILED...', error);
     });
-
-    $("#btn-submit").text('<i class="fas fa-check"></i> Sent');
-    $('#contact-form')[0].reset(); //clear the fields
-
     return false;
 }
