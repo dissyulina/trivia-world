2ee1fefde147e21aaca02fe67a176ebbd26d0aad
Dissy Ulina <dissyulina@gmail.com>
2021-08-10 19:57:37 +0000

    Fix error when a new player is added in Landing page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
-//Variables declaration for Username
+// Variables declaration for Username
 let inputName;
 let userName;
 let data;
 let lastPlayer;
 
-//Check last player on local storage
+// Check last player on local storage
 lastPlayer = localStorage.getItem('player',lastPlayer);
 console.log(lastPlayer === null);
 
-//If there's no lastPlayer saved in the local storage, show up modal to input username
+// If there's no lastPlayer saved in the local storage, show up modal to input username
 if (lastPlayer === null) {
 	$(window).on('load', function() {
 		$("#username-modal").modal('show');
@@ -25,11 +25,6 @@ $('#btn-player').on('click', function(e) {
     $("#username-modal h5").text("Change Player");
     $("#username-modal p").text("Add a new player or input your saved username");
 });
-
-//Show modal on window load
-//$(window).on('load', function() {
-//	$("#username-modal").modal('show');
-//});
 
 //Event listener as the username has been filled and the submit button is clicked
 $('#submit-username').on('click', function(e) {
@@ -55,56 +50,20 @@ function retrieveData() {
 	data = JSON.parse(localStorage.getItem(inputName));
 	console.log(data);
 
-	// Set object back as keys in local storage
-	localStorage.setItem("Sports_score", data.sportsScore);
-	localStorage.setItem("Sports_stars", data.sportsStars);
-	localStorage.setItem("Computer_score", data.computerScore);
-	localStorage.setItem("Computer_stars", data.computerStars);
-	localStorage.setItem("Music_score", data.musicScore);
-	localStorage.setItem("Music_stars", data.musicStars);
-	localStorage.setItem("Film_score", data.filmScore);
-	localStorage.setItem("Film_stars", data.filmStars);
-	localStorage.setItem("Geography_score", data.geographyScore);
-	localStorage.setItem("Geography_stars", data.geographyStars);
-	localStorage.setItem("Science_score", data.scienceScore);
-	localStorage.setItem("Science_stars", data.scienceStars);
-}
-/*
-function saveData() {
-    // All the quiz's datas are put into a new object
-    let datas = new Object();
-    console.log(localStorage.getItem("Sports_stars"));
-    datas = {
-        sportsStars: localStorage.getItem("Sports_stars"),
-        sportsScore: localStorage.getItem("Sports_score"),
-        computerStars: localStorage.getItem("Computer_stars"),
-        computerScore: localStorage.getItem("Computer_score"),
-        musicStars: localStorage.getItem("Music_stars"),
-        musicScore: localStorage.getItem("Music_score"),
-        geographyStars: localStorage.getItem("Geography_stars"),
-        geographyScore: localStorage.getItem("Geography_score"),
-        filmStars: localStorage.getItem("Film_stars"),
-        filmScore: localStorage.getItem("Film_score"),
-        scienceStars: localStorage.getItem("Science_stars"),
-        scienceScore: localStorage.getItem("Science_score"),
+    if(data) {
+        // Set object back as keys in local storage
+        localStorage.setItem("Sports_score", data.sportsScore);
+        localStorage.setItem("Sports_stars", data.sportsStars);
+        localStorage.setItem("Computer_score", data.computerScore);
+        localStorage.setItem("Computer_stars", data.computerStars);
+        localStorage.setItem("Music_score", data.musicScore);
+        localStorage.setItem("Music_stars", data.musicStars);
+        localStorage.setItem("Film_score", data.filmScore);
+        localStorage.setItem("Film_stars", data.filmStars);
+        localStorage.setItem("Geography_score", data.geographyScore);
+        localStorage.setItem("Geography_stars", data.geographyStars);
+        localStorage.setItem("Science_score", data.scienceScore);
+        localStorage.setItem("Science_stars", data.scienceStars);
     }
-    console.log(localStorage.getItem("player"));
-    inputName = localStorage.getItem("player");
-    localStorage.setItem(inputName, JSON.stringify(datas));
-    console.log(JSON.stringify(datas));
 
-    //remove all keys, except the data key inputName
-    window.localStorage.removeItem('Sports_stars');
-    window.localStorage.removeItem('Sports_score');
-    window.localStorage.removeItem('Computer_stars');
-    window.localStorage.removeItem('Computer_score');
-    window.localStorage.removeItem('Music_stars');
-    window.localStorage.removeItem('Music_score');
-    window.localStorage.removeItem('Geography_stars');
-    window.localStorage.removeItem('Geography_score');
-    window.localStorage.removeItem('Film_stars');
-    window.localStorage.removeItem('Film_score');
-    window.localStorage.removeItem('Science_stars');
-    window.localStorage.removeItem('Science_score');
-};
-*/
+}
